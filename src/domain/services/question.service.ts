3b00d408a91ddfa5177ceab740c7799782cb912f
abj1389 <abj_13@hotmail.com>
2023-07-13 22:02:24 +0200

    added question dto

--- src/domain/services/question.service.ts
+++ src/domain/services/question.service.ts
@@ -5,7 +5,31 @@ import { questionOdm } from "../odm/question.odm";
 export const getCurrentQuestions = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const currentVersionQuestions = await questionOdm.getCurrentVersionQuestions();
-    res.status(201).json(currentVersionQuestions);
+    const questionDto = currentVersionQuestions?.map((question: any) => {
+      return {
+        id: question?.id,
+        questionText: question?.questionText,
+        options: question?.options?.map((option: any) => {
+          return {
+            optionText: option.optionText,
+            id: option.id,
+          };
+        }),
+        selectedNumber: {
+          min: question?.selectedNumber?.min,
+          max: question?.selectedNumber?.max,
+          isInverseScore: question?.selectedNumber?.isInverseScore,
+        },
+        category: {
+          id: question.category.id,
+          name: question.category.name,
+        },
+        variant: question?.variant,
+        version: question?.version,
+      };
+    });
+    console.log(questionDto);
+    res.status(201).json(questionDto);
   } catch (error) {
     next(error);
   }
