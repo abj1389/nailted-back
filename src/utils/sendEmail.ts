9830f87334e646609accc82bea1636451f269e68
Fernando <fmoleromartin@gmail.com>
2023-07-17 16:26:47 +0200

    add data info for email

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendResultsMail = async (recipient: string): Promise<void> => {
+export const sendResultsMail = async (recipient: string, dataResults: string): Promise<void> => {
   // Configura los detalles del correo electrónico
   const mailOptions = {
     from: "pruebas@fernandomdev.com",
@@ -19,14 +19,8 @@ export const sendResultsMail = async (recipient: string): Promise<void> => {
     subject: "Aquí tienes los resultados de tu test.",
     html: `
       <h1>Hola</h1>
-      <p>Este es un ejemplo de correo electrónico con formato HTML.</p>
-      <p>Puedes incluir etiquetas HTML como <strong>negrita</strong> o <em>cursiva</em>.</p>
-      <p>También puedes incluir listas:</p>
-      <ul>
-        <li>Elemento 1</li>
-        <li>Elemento 2</li>
-        <li>Elemento 3</li>
-      </ul>
+      <p>Aquí tienes los resultados de tu test:</p>
+      <pre>${dataResults}</pre>
     `,
   };
 
